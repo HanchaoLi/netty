76f9d208412f8e161a47f04f54025dd7879615d8
hanchao.li <hanchao.li@borngroup.com>
2019-06-03 14:24:40 -0400

    NT-002: three ways to write data

--- src/main/java/com/netty/echo/EchoServerHandler.java
+++ src/main/java/com/netty/echo/EchoServerHandler.java
@@ -8,6 +8,20 @@ import io.netty.util.CharsetUtil;
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        /** three ways to write data
+         *
+         * first:
+         * Channel channel = ctx.channel();
+         * channel.writeAndFlush(Unpooled.copiedBuffer("xxxxx", CharsetUtil.UTF-8));
+         *
+         * second:
+         * ChannelPipeline channelPipeline = ctx.pipeline();
+         * channelPipeline.writeAndFlush(Unpooled.copiedBuffer("xxxxx", CharsetUtil.UTF-8));
+         *
+         * thrid:
+         * ctx.writeAndFlush(Unpooled.copiedBuffer("xxxxx", CharsetUtil.UTF-8))
+        */
         ByteBuf data = (ByteBuf) msg;
         System.out.println("server get data: " + data.toString(CharsetUtil.UTF_8));
         ctx.writeAndFlush(data);
